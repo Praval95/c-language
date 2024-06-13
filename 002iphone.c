1b6426257c4b451855801499297dee55706be62f
Praval95 <pravalsahit614@gmail.com>
2024-06-13 12:14:03 +0530

    Add files via upload

---
+++ 002iphone.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int iPhone , cover_case , tamt;
+
+    printf("Enter price of iPhone : ");
+    scanf("%d",&iPhone);
+
+    printf("Enter price of cover_case : ");
+    scanf("%d",&cover_case);
+
+    tamt = iPhone + cover_case ;
+    printf("Total amount is: %d", tamt);
+
+    printf("\n");
+
+    return 0
+
+
+
+}
